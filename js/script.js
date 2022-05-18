6c71af3ae36c9afaddd9f4cf8a1f7a8c1bbd8fc4
VictorShkkn <48925464+VictorShkkn@users.noreply.github.com>
2022-05-19 00:08:35 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let mainScreenTitle = document.querySelector('.section__main-screen__title-animation-before');
+mainScreenTitle.classList.add('section__main-screen__title-animation-after');
+
+let mainScreenDescription = document.querySelector('.section__description-animation-before');
+mainScreenDescription.classList.add('section__description-animation-after');
+
+let mainScreenContact = document.querySelector('.section_main__contact-animation-before')
+mainScreenContact.classList.add('section__description-animation-after');
+
+let mainScreenBtn = document.querySelector('.section_main__button-animation-before')
+mainScreenBtn.classList.add('section_main__button-animation-after');
